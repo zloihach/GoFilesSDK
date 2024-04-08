68893c437e3b9f84e7afb0b8a630642fc49e2876
zloihach <72695485+zloihach@users.noreply.github.com>
2024-04-08 08:19:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from gofile import GoFileClient
 
 if __name__ == "__main__":
     base_url = "https://api.gofile.io"
-    token = "4aBC39u08louNTvzW2P82yIudyannz0z"
+    token = "TOKEN"
     gofile_client = GoFileClient(base_url, token)
 
     servers = gofile_client.get_account_id()
